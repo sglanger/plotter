22caa9bfeddf53063a98cb165fee55bfc6f5fb75
steve.langer@flowsigma.com <steve.langer@flowsigma.com>
2021-07-27 15:06:31 -0500

    initial commit

---
+++ fwatcher.py
@@ -0,0 +1,114 @@
+#!/usr/bin/python3
+
+import time, os
+import subprocess
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+
+	
+class fWatcher(FileSystemEventHandler):
+#############################################
+# from https://stackoverflow.com/questions/18599339/python-watchdog-monitoring-file-for-changes#18599427
+# fixed lots of bugs in above
+# https://programtalk.com/python-examples/watchdog.events.FileSystemEventHandler/
+# look at example 4 above
+
+
+    def __init__(self, path):
+    ###################################
+    # Purpose: setup global class vars
+    ###################################
+      self.last_modified = time.time()   
+      self.path=path
+	
+
+      return
+
+
+    def on_modified(self, event):
+    ###############################################
+    # Purpose: event handler callback
+    # Caller: main event_handler
+    ########################################
+        print (event)
+        res=str(event)
+        res=res[1:res.index(":")]
+       	#print(f'Event type: {event.event_type}  path : {event.src_path}')
+
+        if (res != 'FileModifiedEvent' ) :
+            print ("early return")
+            return
+        else:
+            self.last_modified = time.time()
+            self.call_analytic(event.src_path)
+
+        return
+
+
+    def start(self, handle):
+    ##################################
+    # Purpose: put event handling inside
+    #  a class func
+    ##################################
+
+      observer = Observer()
+      res = observer.schedule(handle, path=self.path, recursive=True)
+      print (res)
+      observer.start()
+
+      try:
+        while True:
+          time.sleep(1)
+      except KeyboardInterrupt:
+        observer.stop()
+        observer.join()
+
+      return
+
+
+    def call_analytic(self, path):
+    ########################################
+    #
+    ######################################
+
+        out_dir = self.path + '/output'
+        event_path = path
+        #print ("in call_ana " + path)
+
+        if 'input' in path :
+          path=self.path  + '/input'
+          cmd_str= self.path + '/files/tools/pyClasses/dumpTags.py'
+
+        if 'output' in path :
+          path = self.path + '/output'
+
+        for f in os.listdir(path):        
+            print (f)
+
+        try:
+          cmd_str = cmd_str  + ' ' + path   # out_dir
+          print (cmd_str )
+          ret = subprocess.check_output(cmd_str)
+        except:
+          print (ret.decode('utf-8'))
+        
+        return 
+
+
+
+if __name__ == "__main__":
+#############################################################
+# Purpose: listen to the  'input' folder, if a file
+#	drops in, call the ANalytic to run on it
+#
+# Caller: this is a service started when the Docker/VM launches
+############################################################
+      os.system('clear')
+
+      ret = subprocess.check_output('pwd')
+      cwd = ret.decode('utf-8') 
+      path = cwd.rstrip()
+      print (cwd)
+
+      event_handler = fWatcher(path)
+      event_handler.start(event_handler)
---
+++ run_dock.sh
@@ -0,0 +1,87 @@
+#!/bin/bash
+
+###############################################
+# Author: SG Langer Jul 2021
+# Purpose: put all the Docker commands to build/run 
+#	ddw-gway in one easy place
+#
+# Notes: If you are building from scratch, run this whole thing
+#	If pulling from Docker hub, just run the RUN and EXEC lines
+##########################################
+
+############## main ###############
+# Purpose: Based on command line arg either
+#		a) build all Docker from scratch or
+#		b) kill running docker or
+#		c) start Docker or
+#		d) restart
+# Caller: user
+###############################
+clear
+
+vmtype=$(sudo virt-what|grep virtualbox)
+if [ $vmtype ] ; then
+	# Vbox
+	echo $vmtype
+	host="10.0.2.15"
+	#host="10.0.2.2"
+else
+	# VMware
+	echo $vmtype
+	host="172.17.0.1"
+fi
+ROOT="/home/site/plotter"
+
+image="jodogne/orthanc-plugins"
+instance="plotter"
+srvc="plotter"
+
+case "$1" in
+	build)
+		# clone the plotter code and site direcotry structure
+		git clone https://github.com/sglanger/$srvc.git
+
+		# install as a service
+  		# https://computingforgeeks.com/how-to-run-java-jar-application-with-systemd-on-linux/
+	#	sudo cp $ROOT/$srvc/$srvc.service /usr/lib/systemd/system/$srvc.service
+	#	sudo systemctl daemon-reload
+	#	sudo systemctl enable $srvc
+
+		$0 start
+	;;
+
+	conn)
+		#sudo docker run -it $instance /bin/bash $image
+		sudo docker exec  -it $instance /bin/bash 
+	;;
+
+
+	conn_r)
+		sudo docker exec -u root -it $instance /bin/bash
+	;;
+
+	restart)
+		$0 stop
+		$0 start
+	;;
+
+	status)
+		sudo systemctl status $srvc 
+	;;
+
+	stop)
+		# stops but does not remove image from DOcker engine
+		sudo systemctl stop $srvc
+	;;
+
+	start)
+		sudo systemctl start $srvc
+	;;
+
+
+	*)
+		echo "invalid option"
+		echo "valid options: build/start/stop/status/conn"
+		exit
+	;;
+esac
