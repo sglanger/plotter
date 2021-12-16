d4e5f1e8d0e8723e41d8c0a7074be87d9329da9d
homeless guy <sglanger@fastmail.com>
2021-12-16 10:46:41 -0600

    put a little omre in sender.py

--- sender.py
+++ sender.py
@@ -27,11 +27,19 @@ if __name__ == "__main__":
   # use cmd line arg to locate projectDir
   if len(sys.argv ) != 2 :
     print ("Incorrect Usage: Must include -input file path- ") 
-    print (">./dispatcher.py file_path ")
+    print (">./sender.py patID ")
     sys.exit(1)
 
-  filePath = sys.argv[1]
-  checkDICOM(filePath)
+  filePath = "pipeline/pending/" + sys.argv[1]
+
+  # now do a send to the Laurel Bridge path to PS360
+  # remember this will be a CIFS push of a PNG or JPG
+  # below DICOM stuff is not applicable
+  LBip = "a.b.c.d"
+  LBport = "104"
+  LBaet = "fake"
+  cmdStr = "dcmsnd " + LBport +"@"+ LBip +":"+ LBport  +" "+ filePath
+  print (cmdStr)
 
   sys.exit()
 
